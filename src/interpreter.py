c172d97f6df2c0dd3172a1da5ef443452b07a595
jerry86547 <clc.dev@protonmail.com>
2021-12-16 19:28:35 +0100

    added interpreter file

---
+++ src/interpreter.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2021 LightningV1p3r
+
+####################
+#Interpreter
+####################
+
+class Interpreter:
+
+    def __init__(self, ast, config) -> None:
+        self.ast = ast
+        self.config = config
+
+    def visit(self):
+        pass
